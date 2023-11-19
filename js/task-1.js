4e5c83fe965cc2f2f61c13ef2b19930481a18e0c
vlkw <138374305+vlkw@users.noreply.github.com>
2023-11-19 16:44:17 +0300

    1

--- js/task-1.js
+++ js/task-1.js
@@ -3,14 +3,23 @@
 function isEnoughCapacity(products, containerSize) {
 
 }
-const atTheOldToad = {
-    potions: [],
-    getPotions() {
-        return "List of all available potions"
-    },
-    addPotion(potionName) {
-        return 'Adding ${potionName}'
-    },
-
+let totalQuantity = 0;
+for (const quantity of Object.values(products)) {
+    totalQuantity += quantity;
 }
-console.log(atTheOldToad.addPotion(loh))
+return totalQuantity <= containerSize;
+console.log(
+    isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+    isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+    isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
+); // true
+
+console.log(
+    isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
+); // false

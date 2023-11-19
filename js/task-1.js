8e378793148daf0453cf9699ba87b211f7546cf0
vlkw <138374305+vlkw@users.noreply.github.com>
2023-11-19 16:38:58 +0300

    s

---
+++ js/task-1.js
@@ -0,0 +1,16 @@
+'use strict'
+
+function isEnoughCapacity(products, containerSize) {
+
+}
+const atTheOldToad = {
+    potions: [],
+    getPotions() {
+        return "List of all available potions"
+    },
+    addPotion(potionName) {
+        return 'Adding ${potionName}'
+    },
+
+}
+console.log(atTheOldToad.addPotion(loh))
